a71211f49eb3a8ad49e0a53ee9d0fcf108db3596
Devon <devondgray28@gmail.com>
2022-06-05 22:26:31 -0500

    adding button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,4 +24,22 @@ $(document).ready(function () {
           $(this).addClass("future").removeClass("past").removeClass("present");
         };
       });
-    
+      
+       //load tasks previously stored to localStorage
+  for (var i = 0; i < workHours.length; i++) {
+    //loop through each work hour
+    $("#hour-" + workHours[i] + " .description").val(localStorage.getItem(workHours[i]));
+  }});
+
+  //listen for button click
+  $(".row .saveBtn").on("click", function (e) {
+    var btn = e.target;
+    var index = btn.dataset.index;
+    // get nearby values to save to local storage
+    var taskText = $("textarea[data-index=" + "'" + index + "'" + "]").val();
+    // use time as key for local storage
+    var time = $(this).parent().attr("id").split("-")[1];
+    // set localStorage with time and textarea
+    localStorage.setItem(time, taskText);
+  });
+});
